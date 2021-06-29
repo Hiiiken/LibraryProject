71a0c01068aabf6dda7732261c01f49a7c3547f5
Hiiiken <hiiiken@live.com>
2021-06-29 19:39:50 +0100

    Initial import

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch } = require('gulp');
+const compileSass = require("gulp-sass")(require("node-sass"));
+
+compileSass.compiler = require('node-sass');
+
+const bundleSass = () => {
+    return src('./scss/**/*.scss')
+    .pipe(compileSass().on('error', compileSass.logError))
+    .pipe(dest('./css/'));
+};
+
+const devWatch = () => {
+    watch('./scss/**/*.scss', bundleSass);
+}
+
+exports.bundleSass = bundleSass;
+exports.devWatch =  devWatch;
