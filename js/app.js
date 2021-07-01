9e07aa8721ac3d4b6e88710e821cb03f7e7e2a54
Hiiiken <hiiiken@live.com>
2021-07-01 18:26:23 +0100

    Capturing user input function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log('Hello there!!')
+// Book constructor
+function Book(title, author, pages, read) {
+   this.title = title,
+   this.author = author,
+   this.pages = pages,
+   this.read = read
+}
+
+// Catching user input
+let bookTitle = document.querySelector('#title');
+let bookAuthor = document.querySelector('#author');
+let bookPages = document.querySelector('#pages');
+let bookStatus = document.querySelector('#read');
+// let bookAddBtn = document.querySelector('#submit');
+let bookForm = document.querySelector('#add-book-form');
+
+let myLibrary = [];
+
+// Function to add books to myLibrary array
+function addBookToLibrary() {
+   const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
+   return myLibrary.push(book);
+}
+
+bookForm.addEventListener('submit', function(e) {
+   e.preventDefault();
+   addBookToLibrary();
+   clearForm();
+})
+
+
+// Function to clear the form after clicking the 'New Book'
+function clearForm() {
+   bookTitle.value = '';
+   bookAuthor.value = '';
+   bookPages.value = '';
+   bookStatus.value = '';
+}
