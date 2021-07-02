80e728083a0197b779bfc229ee956bff20774d02
Hiiiken <hiiiken@live.com>
2021-07-02 21:40:56 +0100

    Adding a function to display data in HTML, and experimenting with popups

--- js/app.js
+++ js/app.js
@@ -26,8 +26,8 @@ bookForm.addEventListener('submit', function(e) {
    e.preventDefault();
    addBookToLibrary();
    clearForm();
+   render();
 })
-
 
 // Function to clear the form after clicking the 'New Book'
 function clearForm() {
@@ -35,4 +35,51 @@ function clearForm() {
    bookAuthor.value = '';
    bookPages.value = '';
    bookStatus.value = '';
+}
+
+// Function to display data in HTML
+let booksList = document.getElementById('books-list-grid');
+function render() {
+   booksList.innerHTML = '';
+   myLibrary.forEach((book) => {
+      let htmlBook = `
+      <div class="col col-4 col-lg">
+         <div class="book-card">
+            <h4 class="title">${book.title}</h4>
+            <p class="author">${book.author}</p>
+            <p class="pages">${book.pages}</p>
+            <p class="status">${book.read}</p>
+            <button class="btn btn-edit">Edit</button>
+            <button class="btn btn-delete">Delete</button>
+         </div>
+      </div>
+      `;
+      booksList.insertAdjacentHTML('afterbegin', htmlBook);
+   });
+}
+
+
+
+// Popups
+const openPopupButtons = document.querySelectorAll('[data-popup-target]');
+const closePopupButtons = document.querySelectorAll('[data-close-popup]');
+
+openPopupButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      const popup = document.querySelector(button.dataset.popupTarget);
+      togglePopup(popup);
+   })
+})
+
+closePopupButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      const popup = button.closest('.popup');
+      togglePopup(popup);
+   })
+})
+
+function togglePopup(popup) {
+   // document.getElementById('popup-1').classList.toggle('active');
+   if (popup == null) return;
+   popup.classList.toggle('active');
 }
